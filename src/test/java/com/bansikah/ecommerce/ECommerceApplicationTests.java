4bdfdf922fc07c8d6523975794fc2dff8a595fe3
Bansikah <tandapnoelbansikah@gmail.com>
2024-03-15 17:06:40 +0100

    add dependecies for test containers

--- src/test/java/com/bansikah/ecommerce/ECommerceApplicationTests.java
+++ src/test/java/com/bansikah/ecommerce/ECommerceApplicationTests.java
@@ -1,13 +1,38 @@
 package com.bansikah.ecommerce;
 
+import com.bansikah.ecommerce.service.ProductService;
+import net.bytebuddy.utility.dispatcher.JavaDispatcher;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class ECommerceApplicationTests {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:16-alpine"
+    );
+
+    @Autowired
+   ProductService productService;
+
+    @BeforeEach
+    void setUp() {
+          productService =  new ProductService(
+                  postgres.getJdbcUrl()
+          );
+    }
+
 
     @Test
     void contextLoads() {
+        System.out.println("Hello, World!");
     }
 
 }
